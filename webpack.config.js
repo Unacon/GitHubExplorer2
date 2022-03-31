94a94a3b94875223fda80b0c6e0f5b89e2cf4d3b
Joao Vitor Pinheiro <joaovitor_892@hotmail.com>
2022-03-30 21:30:44 -0300

    REct refresh web

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,54 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebPackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const isDevolopment = process.env.NODE_ENV === 'production';
+const isDevolopment = process.env.NODE_ENV === "production";
 
-module.exports ={
-    mode : isDevolopment ? 'development' : 'production',
-    devtool: isDevolopment ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname,'src','index.jsx'),
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
+module.exports = {
+  mode: isDevolopment ? "development" : "production",
+  devtool: isDevolopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
     },
-    resolve: {
-        extensions:['.js','.jsx']
-    },
-    devServer:{
-        static: {
-            directory: path.resolve(__dirname,'public'),
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    isDevolopment && new ReactRefreshWebPackPlugin(),
+  ].filter(Boolean),
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve("babel-loader"),
+          options: {
+            plugins: [
+              isDevolopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
           },
-    },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: path.resolve(__dirname,'public','index.html')
-        })
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    performance: { 
-        hints: false 
-    },
-    module:{
-        rules:[
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader','sass-loader'],
-            }
-        ],
-    }
+  },
 };
